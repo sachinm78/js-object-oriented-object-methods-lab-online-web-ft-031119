9c1debec641de9e471f6b399495af9139a8cd7bb
Sachin Mahajan <sachinm78@gmail.com>
2019-06-13 22:13:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ function BoardMember(name, homeState, training){
     return 'I like to help people.'
   }
   this.releasePressStatement = function(){
+    return 'You will see great things from Scuber.'
+  }
+  this.sayHi = function(){
     return ''
   }
 }
